e48c419eb855c214f9a7442548e41585d63f4fc7
i00lii <dydaniil@yandex.ru>
2022-10-03 00:41:35 +0300

    Update 00002.add-two-numbers.cs

--- 00002.add-two-numbers.cs
+++ 00002.add-two-numbers.cs
@@ -1,2 +1,115 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode resultHead = default;
+        ListNode resultTail = default;
+        int overflow = default;
+        
+        while(l1 != default || l2 != default)
+        {
+            int sum = (l1?.val ?? default) + (l2?.val ?? default) + overflow;
+            
+            overflow = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            
+            if (resultHead == default)
+            {
+                resultHead = newNode;
+                resultTail = newNode;  
+            }
+            else
+            {
+                resultTail.next = newNode;
+                resultTail = newNode;
+            }
+            
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+        
+        if (overflow != default)
+        {
+            resultTail.next = new ListNode(overflow);
+        }
+        
+        return resultHead;
+    }
+}
 
-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
