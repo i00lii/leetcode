6304a4c1f329f1f0ae806bf70b58f2cc7b7cd3f9
i00lii <dydaniil@yandex.ru>
2022-10-03 00:09:03 +0300

    Update 1.two-sum.cs

--- 1.two-sum.cs
+++ 1.two-sum.cs
@@ -1,2 +1,20 @@
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        Dictionary<int, int> cache = new Dictionary<int, int>();
 
-
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            int current = nums[idx];
+            int lookup = target - current;
+
+            if (cache.TryGetValue(lookup, out int lookupIdx))
+                return new int[] {lookupIdx, idx};
+
+            cache.TryAdd(current, idx);
+        }
+
+        throw null;
+    }
+}
