53d540187693927917921dcc7cbf97419c82ff6d
i00lii <dydaniil@yandex.ru>
2022-10-08 02:02:27 +0300

    Update 00003.longest-substring-without-repearing.cs

--- 00003.longest-substring-without-repearing.cs
+++ 00003.longest-substring-without-repearing.cs
@@ -1,2 +1,19 @@
-
-
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s) 
+    {
+        short[] charPositions = new short[128];
+        charPositions.AsSpan().Fill(-1);
+        
+        int start = 0;
+        int result = 0;
+        for (short i = 0; i < s.Length; ++i)
+        {
+            ref short position = ref charPositions[s[i]];
+            start = Math.Max(start, position + 1);
+            position = i;
+            result = Math.Max(result, i - start + 1);
+        }
+        return result;
+    }
+}
