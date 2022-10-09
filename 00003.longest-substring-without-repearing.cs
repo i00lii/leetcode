5d1ab09f96cccfe562b71bffb2399d92563cb57e
i00lii <dydaniil@yandex.ru>
2022-10-09 17:37:59 +0300

    Update 00003.longest-substring-without-repearing.cs

--- 00003.longest-substring-without-repearing.cs
+++ 00003.longest-substring-without-repearing.cs
@@ -1,19 +1,17 @@
-public class Solution
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+public int LengthOfLongestSubstring(string s)    
 {
-    public int LengthOfLongestSubstring(string s) 
-    {
-        short[] charPositions = new short[128];
-        charPositions.AsSpan().Fill(-1);
+    short[] charPositions = new short[128];
+    charPositions.AsSpan().Fill(-1);
         
-        int start = 0;
-        int result = 0;
-        for (short i = 0; i < s.Length; ++i)
-        {
-            ref short position = ref charPositions[s[i]];
-            start = Math.Max(start, position + 1);
-            position = i;
-            result = Math.Max(result, i - start + 1);
-        }
-        return result;
+    int start = 0;
+    int result = 0;
+    for (short i = 0; i < s.Length; ++i)
+    {
+        ref short position = ref charPositions[s[i]];
+        start = Math.Max(start, position + 1);
+        position = i;
+        result = Math.Max(result, i - start + 1);
     }
+    return result;
 }
