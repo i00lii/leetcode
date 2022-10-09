690d315b6059903e60ee957eebe974886152e366
i00lii <dydaniil@yandex.ru>
2022-10-09 17:37:00 +0300

    Update 00002.add-two-numbers.cs

--- 00002.add-two-numbers.cs
+++ 00002.add-two-numbers.cs
@@ -9,43 +9,43 @@
  *     }
  * }
  */
-public class Solution
+
+// https://leetcode.com/problems/add-two-numbers/description/
+public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
 {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+  
+    ListNode resultHead = default;
+    ListNode resultTail = default;
+    int overflow = default;
+        
+    while(l1 != default || l2 != default)
     {
-        ListNode resultHead = default;
-        ListNode resultTail = default;
-        int overflow = default;
-        
-        while(l1 != default || l2 != default)
+        int sum = (l1?.val ?? default) + (l2?.val ?? default) + overflow;
+          
+        overflow = sum / 10;
+        ListNode newNode = new ListNode(sum % 10);
+          
+        if (resultHead == default)
         {
-            int sum = (l1?.val ?? default) + (l2?.val ?? default) + overflow;
-            
-            overflow = sum / 10;
-            ListNode newNode = new ListNode(sum % 10);
-            
-            if (resultHead == default)
-            {
-                resultHead = newNode;
-                resultTail = newNode;  
-            }
-            else
-            {
-                resultTail.next = newNode;
-                resultTail = newNode;
-            }
-            
-            l1 = l1?.next;
-            l2 = l2?.next;
+            resultHead = newNode;
+            resultTail = newNode;  
         }
-        
-        if (overflow != default)
+        else
         {
-            resultTail.next = new ListNode(overflow);
+            resultTail.next = newNode;
+            resultTail = newNode;
         }
-        
-        return resultHead;
+          
+        l1 = l1?.next;
+        l2 = l2?.next;
     }
+        
+    if (overflow != default)
+    {
+        resultTail.next = new ListNode(overflow);
+    }
+        
+    return resultHead;
 }
 
 
