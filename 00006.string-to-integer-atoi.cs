162e08d612ced9e4c0cb17e14926bcf3bcd06ca3
i00lii <dydaniil@yandex.ru>
2022-10-09 17:19:53 +0300

    Update 00006.string-to-integer-atoi.cs

--- 00006.string-to-integer-atoi.cs
+++ 00006.string-to-integer-atoi.cs
@@ -1,2 +1,38 @@
+public int MyAtoi(string s)
+{
+    int result = 0;
+    int multiplier = 1;
+    ReadOnlySpan<char> span = s.AsSpan().Trim();
 
-
+    if (span.IsEmpty)
+    {
+        return result;
+    }
+
+    if (span[0] is char first && (first == '+' || first == '-'))
+    {
+        span = span[1..];
+        multiplier = first == '-' ? -1 : 1;
+    }
+
+    int length = 0;
+    while(length < span.Length && span[length] is char x && char.IsDigit(x))
+    {
+        length++;
+    }
+
+    if(length > 0)
+    {
+        span = span[0..length];
+        if (int.TryParse(span, out int value))
+        {
+            result = value * multiplier;
+        }
+        else
+        {
+            result = multiplier > 0 ? int.MaxValue : int.MinValue;
+        }            
+    }
+
+    return result;
+}
