8aa01f56c7008d40153195906a39a006c6877cc1
i00lii <dydaniil@yandex.ru>
2022-10-10 00:38:32 +0300

    Update 00009.palindrome-number.cs

--- 00009.palindrome-number.cs
+++ 00009.palindrome-number.cs
@@ -1,2 +1,29 @@
+// https://leetcode.com/problems/palindrome-number/description/
+public bool IsPalindrome(int x)
+{
+    if (x < 0)
+        return false;
 
-
+    if (x == 0)
+        return true;
+
+    int lastIdx = (int)Math.Floor(Math.Log10(x) + 1) - 1;
+    int head = x;
+    int tail = x;
+
+    for(int idx = 0; idx <= lastIdx / 2; idx++)
+    {    
+        int headMultiplier = (int)Math.Pow(10, lastIdx - idx);
+        int headDigit = (int)Math.Floor((double)head / headMultiplier);
+
+        int tailDigit = tail % 10;
+
+        head = head - headMultiplier * (int)Math.Floor((double)head / headMultiplier);
+        tail /= 10;
+
+        if (headDigit != tailDigit)
+            return false;
+    }
+
+    return true;
+}
