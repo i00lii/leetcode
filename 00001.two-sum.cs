64d395de8d3347696c9ab9d1dc93bb9cb5b9aa10
i00lii <dydaniil@yandex.ru>
2022-10-09 17:34:20 +0300

    Update 00001.two-sum.cs

--- 00001.two-sum.cs
+++ 00001.two-sum.cs
@@ -1,21 +1,18 @@
-// https://leetcode.com/problems/two-sum/
-public class Solution 
+// https://leetcode.com/problems/two-sum/description/
+public int[] TwoSum(int[] nums, int target)
 {
-    public int[] TwoSum(int[] nums, int target) 
+    Dictionary<int, int> cache = new Dictionary<int, int>();
+    
+    for (int idx = 0; idx < nums.Length; idx++)
     {
-        Dictionary<int, int> cache = new Dictionary<int, int>();
-
-        for (int idx = 0; idx < nums.Length; idx++)
-        {
-            int current = nums[idx];
-            int lookup = target - current;
-
-            if (cache.TryGetValue(lookup, out int lookupIdx))
-                return new int[] {lookupIdx, idx};
-
-            cache.TryAdd(current, idx);
-        }
-
-        throw null;
+        int current = nums[idx];
+        int lookup = target - current;
+        
+        if (cache.TryGetValue(lookup, out int lookupIdx)
+            return new int[] {lookupIdx, idx};
+        
+        cache.TryAdd(current, idx);
     }
+
+    throw null;
 }
