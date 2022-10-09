f84300da765c9c73af4963b73cce8dbde4af0977
i00lii <dydaniil@yandex.ru>
2022-10-09 17:32:58 +0300

    Update 00007.reverse-integer.cs

--- 00007.reverse-integer.cs
+++ 00007.reverse-integer.cs
@@ -1,2 +1,18 @@
+// https://leetcode.com/problems/reverse-integer/description/
+public int Reverse(int x) 
+{
+    const int _minValue = int.MinValue / 10;
+    const int _maxValue = int.MaxValue / 10;
+  
+    int rev = 0;
 
-
+    while (x != 0) 
+    {
+        int pop = x % 10;
+        x /= 10;
+        if (rev > _maxValue || (rev == _maxValue && pop > 7)) return 0;
+        if (rev < _minValue || (rev == _minValue && pop < -8)) return 0;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
