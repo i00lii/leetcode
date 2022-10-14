c73403f227a7331969dc209df32196ff317657b6
i00lii <dydaniil@yandex.ru>
2022-10-15 00:46:05 +0300

    Update 00011.container-with-most-water.cs

--- 00011.container-with-most-water.cs
+++ 00011.container-with-most-water.cs
@@ -1,1 +1,29 @@
+// https://leetcode.com/problems/container-with-most-water/description/
+public int MaxArea(int[] height) 
+{
+    int x = 0;
+    int y = height.Length - 1;
 
+    int area = 0;
+
+    while (x != y)
+    {
+        int valueX = height[x];
+        int valueY = height[y];
+        int size = y - x;
+
+        if (valueX > valueY)
+        {
+            area = Math.Max(valueY * size, area);
+            y--;
+        }
+        else 
+        {
+            area = Math.Max(valueX * size, area);
+            x++;
+        }
+
+    }
+
+    return area;
+}
