9f54970ffa0b3b8d2c51468b42a5215ba7e74f9b
i00lii <dydaniil@yandex.ru>
2022-10-16 12:22:18 +0300

    Update 00013.roman-to-integer.cs

--- 00013.roman-to-integer.cs
+++ 00013.roman-to-integer.cs
@@ -1,2 +1,34 @@
+// https://leetcode.com/problems/roman-to-integer/description/
+public int RomanToInt(string s) 
+{
+    int result = 0;
+    int previousValue = 0;
 
-
+    for(int idx = s.Length -1; idx >= 0; idx--)
+    {
+        int currentValue = s[idx] switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,  
+            _ => default    
+        };
+
+        if (currentValue < previousValue)
+        {
+            result -= currentValue;
+        }
+        else
+        {
+            result += currentValue;
+        }
+
+        previousValue = currentValue;
+    }
+
+    return result;
+}
