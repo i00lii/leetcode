73ed6f32af525e9415596186a64162ca6c32d74c
i00lii <dydaniil@yandex.ru>
2022-10-16 12:40:58 +0300

    Update 00013.longest-common-prefix.cs

--- 00013.longest-common-prefix.cs
+++ 00013.longest-common-prefix.cs
@@ -1,2 +1,21 @@
+// https://leetcode.com/problems/longest-common-prefix/description/
+public string LongestCommonPrefix(string[] strs) 
+{
+    for(int idx = 0;;idx++)
+    {
+        if (idx >= strs[0].Length)
+            return strs[0].Substring(0, idx);
 
-
+        char current = strs[0][idx];
+
+        for(int jdx = 1; jdx < strs.Length; jdx++)
+        {
+            string currentString = strs[jdx];
+
+            if (idx >= currentString.Length || currentString[idx] != current)
+            {
+                return strs[0].Substring(0, idx);
+            }
+        }
+    }
+}
