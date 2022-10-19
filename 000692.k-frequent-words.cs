df3d9d1eaf1542404baf5db929212ece55da0ab5
i00lii <dydaniil@yandex.ru>
2022-10-19 21:36:39 +0300

    Update 000692.k-frequent-words.cs

--- 000692.k-frequent-words.cs
+++ 000692.k-frequent-words.cs
@@ -1,2 +1,11 @@
-
-
+// https://leetcode.com/problems/top-k-frequent-words/description/
+// beware power of the linq
+public IList<string> TopKFrequent(string[] words, int k) 
+    => words
+    .GroupBy(item => item)
+    .Select(item => (Count: item.Count(), Value: item.Key))
+    .OrderByDescending(item => item.Count)
+    .ThenBy(item => item.Value)
+    .Take(k)
+    .Select(item => item.Value)
+    .ToArray();
