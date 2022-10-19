92126b6337921ebf9d794ea9f6634adfde012113
i00lii <dydaniil@yandex.ru>
2022-10-19 21:56:04 +0300

    Update 00058.length-of-last-word.cs

--- 00058.length-of-last-word.cs
+++ 00058.length-of-last-word.cs
@@ -1,2 +1,29 @@
+// https://leetcode.com/problems/length-of-last-word/description/
+public int LengthOfLastWord(string s) 
+{
+    int lastIdx = -1;
+    int beforeFirstIdx = -1;
 
-
+    for (int idx = s.Length - 1; idx >= 0; idx--)
+    {
+        bool isWhitespace = char.IsWhiteSpace(s[idx]);
+
+        if (lastIdx == -1)
+        {
+            if (!isWhitespace)
+            {
+                lastIdx = idx;    
+            }
+        }
+        else 
+        {
+            if (isWhitespace)
+            {
+                beforeFirstIdx = idx;
+                break;
+            }
+        }
+    }
+
+    return lastIdx - beforeFirstIdx;
+}
