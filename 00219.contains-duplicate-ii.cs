e2d05fa1ee8919e32aebe499dc8e70d69ebab8f3
i00lii <dydaniil@yandex.ru>
2022-10-22 17:20:25 +0300

    Update 00219.contains-duplicate-ii.cs

--- 00219.contains-duplicate-ii.cs
+++ 00219.contains-duplicate-ii.cs
@@ -1,2 +1,31 @@
+// https://leetcode.com/problems/contains-duplicate-ii/description/
+public bool ContainsNearbyDuplicate(int[] nums, int k) 
+{
+    if (k == 0)
+        return false;
 
-
+    HashSet<int> lookup= new HashSet<int>(k);
+
+    for(int idx = 0; idx < Math.Min(k, nums.Length); idx++)
+    {
+        if (!lookup.Add(nums[idx]))
+        {
+            return true;
+        }
+    }
+
+    for(int idx = k; idx < nums.Length; idx++)
+    {
+        int current = nums[idx];
+        int old = nums[idx - k];
+
+        if (!lookup.Add(current))
+        {
+            return true;
+        }
+
+        lookup.Remove(old);
+    }
+
+    return false;
+}
