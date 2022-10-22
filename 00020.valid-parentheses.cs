2070917a5c0f9100c7b77372fddc14da542d2eb0
i00lii <dydaniil@yandex.ru>
2022-10-23 00:52:02 +0300

    Update 00020.valid-parentheses.cs

--- 00020.valid-parentheses.cs
+++ 00020.valid-parentheses.cs
@@ -1,2 +1,41 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+public class Solution 
+{
+    public bool IsValid(string s) 
+    {
+        Stack<char> buffer = new Stack<char>();
+        
+        foreach(char value in s)
+        {
+            switch(value)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    buffer.Push(value);
+                    break;
+                    
+                case ')':                    
+                    if (buffer.Count == 0 || buffer.Pop() != '(')
+                        return false;
 
-
+                    break;
+                case ']':                    
+                    if (buffer.Count == 0 || buffer.Pop() != '[')
+                        return false;
+
+                    break;
+                case '}':
+                    if (buffer.Count == 0 || buffer.Pop() != '{')
+                        return false;
+
+                    break;
+                
+                default:
+                    return false;
+            }
+        }
+        
+        return buffer.Count == 0;
+    }
+}
