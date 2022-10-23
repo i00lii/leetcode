99394d4f5adc58b9e91ecac451bd0c30d780e4b0
i00lii <dydaniil@yandex.ru>
2022-10-23 18:44:58 +0300

    Update 00645.set-mismatch.cs

--- 00645.set-mismatch.cs
+++ 00645.set-mismatch.cs
@@ -1,2 +1,33 @@
+// https://leetcode.com/problems/set-mismatch/description/
+public class Solution 
+{
+    public int[] FindErrorNums(int[] nums) 
+    {
+        int[] buffer = new int[nums.Length];
+        int[] result = new int[2];
 
-
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            buffer[nums[idx] - 1]++;
+        }
+        
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            if (buffer[idx] == 0)
+            {
+                result[1] = idx + 1;
+                
+                if (result[0] != default)
+                    break;
+            }
+            else if (buffer[idx] == 2)
+            {
+                result[0] = idx + 1;
+                
+                if (result[1] != default)
+                    break;
+            }
+        }
+        return result;
+    }
+}
