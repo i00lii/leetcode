12c527326e1ee7401f67a991581e0d1af254757c
i00lii <dydaniil@yandex.ru>
2022-10-25 00:24:44 +0300

    Update 00021.merge-two-sorted-lists.cs

--- 00021.merge-two-sorted-lists.cs
+++ 00021.merge-two-sorted-lists.cs
@@ -1,2 +1,39 @@
-
-
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) 
+    {
+        ListNode root = new ListNode();
+        ListNode current = root;
+        
+        while (list1 != default && list2 != default)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            
+            current = current.next;
+        }
+        
+        current.next = list1 ?? list2;
+        return root.next; 
+    }
+}
