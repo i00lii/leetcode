b5fc1eafcc4ae36285a72955f04f0ac2d56444bb
i00lii <dydaniil@yandex.ru>
2022-10-27 01:10:33 +0300

    Update 01115.print-foobar-alternately.cs

--- 01115.print-foobar-alternately.cs
+++ 01115.print-foobar-alternately.cs
@@ -1,2 +1,41 @@
+// https://leetcode.com/problems/print-foobar-alternately/description/
+using System.Threading;
 
-
+public class FooBar 
+{
+    private int n;
+    private readonly SemaphoreSlim _foo = new SemaphoreSlim(1, 1);
+    private readonly SemaphoreSlim _bar = new SemaphoreSlim(0, 1);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void Foo(Action printFoo) 
+    {
+        
+        for (int i = 0; i < n; i++) 
+        {
+            _foo.Wait();
+            
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            
+            _bar.Release();
+        }
+    }
+
+    public void Bar(Action printBar)
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            _bar.Wait();
+            
+            // printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            
+            _foo.Release();
+        }
+    }
+}
