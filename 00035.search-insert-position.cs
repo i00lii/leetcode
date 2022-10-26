bff7d20d4b50e549350e97ae78b5525744e220a6
i00lii <dydaniil@yandex.ru>
2022-10-26 23:48:34 +0300

    Update 00035.search-insert-position.cs

--- 00035.search-insert-position.cs
+++ 00035.search-insert-position.cs
@@ -1,2 +1,39 @@
-
-
+// https://leetcode.com/problems/search-insert-position/description/
+public class Solution 
+{
+    public int SearchInsert(int[] nums, int target) 
+    {
+        int xIdx = 0;
+        int yIdx = nums.Length - 1;
+        
+        if (target <= nums[0])
+            return 0;
+        
+        if (target == nums[yIdx])
+            return yIdx;
+        
+        if (target > nums[yIdx])
+            return ++yIdx;
+        
+        while (yIdx - xIdx > 1)
+        {
+            int middleIdx = xIdx + (yIdx - xIdx) / 2;
+            int middleValue = nums[middleIdx];
+            
+            if (middleValue == target)
+            {
+                return middleIdx;
+            }
+            else if (target > middleValue)
+            {
+                xIdx = middleIdx;
+            }
+            else
+            {
+                yIdx = middleIdx;
+            }
+        }
+        
+        return yIdx;
+    }
+}
