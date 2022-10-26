1ebd6afda188aab38c9a68f5202185de8deffcb3
i00lii <dydaniil@yandex.ru>
2022-10-26 19:43:59 +0300

    Update 00523.continuous-subarray-sum.cs

--- 00523.continuous-subarray-sum.cs
+++ 00523.continuous-subarray-sum.cs
@@ -1,2 +1,28 @@
-
-
+// https://leetcode.com/problems/continuous-subarray-sum/description/
+public class Solution 
+{
+    public bool CheckSubarraySum(int[] nums, int k) 
+    {
+        Dictionary<int, int> buffer = new Dictionary<int, int>();
+        buffer[0] = 0;
+        
+        int totalAmount = 0;
+        
+        for(int idx = 0; idx < nums.Length; idx++)
+        {
+            totalAmount += nums[idx];
+            int reminder = totalAmount % k;
+            
+            if (!buffer.ContainsKey(reminder))
+            {
+                buffer[reminder] = idx + 1;
+            }
+            else if (buffer[reminder] < idx)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
