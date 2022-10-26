beebb25f503aa0b2f8651f012ab87e920eae6768
i00lii <dydaniil@yandex.ru>
2022-10-26 19:57:20 +0300

    Update 00026.remove-duplicates-from-sorted-array.cs

--- 00026.remove-duplicates-from-sorted-array.cs
+++ 00026.remove-duplicates-from-sorted-array.cs
@@ -1,2 +1,26 @@
-
-
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+public class Solution 
+{
+    public int RemoveDuplicates(int[] nums) 
+    {
+        int old = nums[0];
+        
+        int shift = 0;        
+        for(int idx = 1; idx < nums.Length; idx++)
+        {
+            int current = nums[idx];
+        
+            if (current == old)
+            {
+                shift++;
+            }
+            else
+            {
+                old = current;
+                nums[idx - shift] = current;
+            }
+        }
+        
+        return nums.Length - shift;
+    }
+}
