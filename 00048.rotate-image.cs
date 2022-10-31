2d8c65ef93199a17a6cccc726c3adef8cd9bbd51
i00lii <dydaniil@yandex.ru>
2022-11-01 01:53:19 +0300

    Update 00048.rotate-image.cs

--- 00048.rotate-image.cs
+++ 00048.rotate-image.cs
@@ -1,2 +1,32 @@
+// https://leetcode.com/problems/rotate-image/description/
+public class Solution 
+{
+    public void Rotate(int[][] matrix) 
+    {
+        int lastIdx = matrix.Length - 1;
+        int half = matrix.Length / 2;
+        
+        for (int yIdx = 0; yIdx < half; yIdx++)
+        {
+            for (int xIdx = yIdx; xIdx < lastIdx - yIdx; xIdx++)
+            {
+                int current = matrix[yIdx][xIdx];
 
-
+                for(int cornerIdx = 0; cornerIdx < 4; cornerIdx++)
+                {
+                    int nextXIdx = lastIdx - yIdx;
+                    int nextYIdx = xIdx;
+
+                    ref int next = ref matrix[nextYIdx][nextXIdx];
+                    int buffer = next;
+
+                    next = current;
+                    current = buffer;
+
+                    xIdx = nextXIdx;
+                    yIdx = nextYIdx;
+                }
+            }
+        }
+    }
+}
