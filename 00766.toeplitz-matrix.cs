98955d021d29a4da06190d4796066f00f70aebc9
i00lii <dydaniil@yandex.ru>
2022-10-31 16:28:32 +0300

    Update 00766.toeplitz-matrix.cs

--- 00766.toeplitz-matrix.cs
+++ 00766.toeplitz-matrix.cs
@@ -1,2 +1,23 @@
+// https://leetcode.com/problems/toeplitz-matrix/description
+public class Solution 
+{
+    public bool IsToeplitzMatrix(int[][] matrix)
+    {
+        int x = matrix[0].Length;
+        int y = matrix.Length;
 
-
+        for (int yIdx = 1; yIdx < y; yIdx++)
+        {
+            int[] a = matrix[yIdx - 1];
+            int[] b = matrix[yIdx];
+
+            for (int xIdx = 1; xIdx < x; xIdx++)
+            {
+                if (a[xIdx - 1] != b[xIdx])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
