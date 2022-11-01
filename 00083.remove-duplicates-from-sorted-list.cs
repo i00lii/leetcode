a4e9f058214cade41f1741cace3446fd0e394f70
i00lii <dydaniil@yandex.ru>
2022-11-02 01:40:10 +0300

    Update 00083.remove-duplicates-from-sorted-list.cs

--- 00083.remove-duplicates-from-sorted-list.cs
+++ 00083.remove-duplicates-from-sorted-list.cs
@@ -1,2 +1,36 @@
-
-
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public ListNode DeleteDuplicates(ListNode head) 
+    {
+        if (head == default)
+            return head;
+        
+        ListNode originalHead = head;
+        
+        while (head.next is {} next)
+        {
+            if (head.val == next.val)
+            {
+                head.next = next.next;
+            }
+            else 
+            {
+                head = head.next;
+            }            
+        }
+        
+        return originalHead;
+    }
+}
