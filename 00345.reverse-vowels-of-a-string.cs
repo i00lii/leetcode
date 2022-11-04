7179bdab11d207c0a024a3edf6187a7c901c9b7f
i00lii <dydaniil@yandex.ru>
2022-11-04 14:11:41 +0300

    Update 00345.reverse-vowels-of-a-string.cs

--- 00345.reverse-vowels-of-a-string.cs
+++ 00345.reverse-vowels-of-a-string.cs
@@ -1,2 +1,47 @@
+// https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+public class Solution 
+{
+    public string ReverseVowels(string s) 
+    {
+        if (string.IsNullOrEmpty(s))
+            return s;
 
-
+        char[] buffer = s.ToCharArray();
+
+        int begin = 0;
+        int end = buffer.Length - 1;
+
+        while (true)
+        {
+            while(!IsVovel(s[begin]) && begin < end)
+            {
+                begin++;
+            }
+
+            while(!IsVovel(s[end]) && begin < end)
+            {
+                end--;
+            }
+
+            if (begin >= end)
+                break;
+
+            char temp = buffer[begin];
+            buffer[begin] = buffer[end];
+            buffer[end] = temp;
+
+            begin++;
+            end--;
+        }
+
+        return new string(buffer);
+    }
+
+    private static bool IsVovel(char value)
+        => value switch
+        {
+            'a' or 'e' or 'i' or 'u' or 'o'=> true,
+            'A' or 'E' or 'I' or 'U' or 'O'=> true,
+            _ => default
+        };
+}
