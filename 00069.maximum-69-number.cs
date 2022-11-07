badf1ebeb5da71989cfd61761b7ae64768ed4666
i00lii <dydaniil@yandex.ru>
2022-11-07 23:59:43 +0300

    Update 00069.maximum-69-number.cs

--- 00069.maximum-69-number.cs
+++ 00069.maximum-69-number.cs
@@ -1,2 +1,26 @@
+// maximum-69-number
+public class Solution 
+{
+    public int Maximum69Number(int num) 
+    {
+        int buffer = num;
+        
+        int multiplier = 1;
+        int max = num;
+        
+        while (buffer > 0)
+        {
+            int value = buffer % 10;
 
-
+            if (value == 6)
+            {
+                max = num + 3 * multiplier;
+            }
+
+            buffer /= 10;
+            multiplier *= 10;
+        }
+
+        return max;
+    }
+}
