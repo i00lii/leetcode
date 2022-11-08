bbb980a2afe3b7e7de8e84c16166762c5b588c8e
i00lii <dydaniil@yandex.ru>
2022-11-08 13:59:53 +0300

    Update 01544.make-the-string-great.cs

--- 01544.make-the-string-great.cs
+++ 01544.make-the-string-great.cs
@@ -1,2 +1,27 @@
+// https://leetcode.com/problems/make-the-string-great/
+public class Solution 
+{
+    public string MakeGood(string s) 
+    {
+        StringBuilder buffer = new StringBuilder(s);
 
-
+        int idx = 0;
+        while(idx < buffer.Length - 1)
+        {
+            char x = buffer[idx];
+            char y = buffer[idx + 1];
+
+            if (char.ToLower(x) == char.ToLower(y) && x != y)
+            {
+                buffer.Remove(idx, 2);
+                idx = Math.Max(idx - 1, 0);
+            }
+            else 
+            {
+                idx++;
+            }
+        }
+        
+        return buffer.ToString();
+    }
+}
