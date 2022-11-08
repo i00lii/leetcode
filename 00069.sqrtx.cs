9f430a79ef9e92a4b135a54382d4ce65d4e13e6d
i00lii <dydaniil@yandex.ru>
2022-11-09 02:30:23 +0300

    Update 00069.sqrtx.cs

--- 00069.sqrtx.cs
+++ 00069.sqrtx.cs
@@ -1,2 +1,24 @@
+// https://leetcode.com/problems/sqrtx/submissions/839739978/
+public class Solution 
+{
+    public int MySqrt(int x)
+    {
+        if (x <= 1)
+            return x;
 
-
+        int left = 1;
+        int right = Math.Min(x / 2, 46340);
+
+        while (right >= left)
+        {
+            int middle = (left + right) / 2;
+            int target = middle * middle;
+            
+            if (target < x) left = middle + 1; 
+            else if (target > x) right = middle - 1; 
+            else return middle;
+        }
+
+        return right;
+    }
+}
