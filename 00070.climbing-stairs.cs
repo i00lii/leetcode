56ec31c2ddda9dd540da8e14c03a8c0cb38ac694
i00lii <dydaniil@yandex.ru>
2022-11-10 00:50:46 +0300

    Update 00070.climbing-stairs.cs

--- 00070.climbing-stairs.cs
+++ 00070.climbing-stairs.cs
@@ -1,2 +1,18 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+public class Solution 
+{
+    private static readonly int[] _buffer = new int[46];
 
-
+    static Solution()
+    {
+        _buffer[1] = 1;
+        _buffer[2] = 2;
+
+        for (int idx = 3; idx < _buffer.Length; idx++)
+        {
+            _buffer[idx] = _buffer[idx - 1] + _buffer[idx - 2];
+        }
+    }
+
+    public int ClimbStairs(int n) => _buffer[n];
+}
