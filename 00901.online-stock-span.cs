0bf735029d21b593ac3d490ecfd5e43256928e08
i00lii <dydaniil@yandex.ru>
2022-11-09 19:40:21 +0300

    Update 00901.online-stock-span.cs

--- 00901.online-stock-span.cs
+++ 00901.online-stock-span.cs
@@ -1,2 +1,20 @@
+// https://leetcode.com/problems/online-stock-span/description/
+public class StockSpanner 
+{
+    private readonly Stack<(int, int)> _stack;
+    public StockSpanner() => _stack = new Stack<(int, int)>();
+    
+    public int Next(int price) 
+    {
+        int strike = 1;
 
-
+        while(_stack.TryPeek(out (int Value, int Strike) peeked) && peeked.Value <= price)
+        {
+            _stack.Pop();
+            strike += peeked.Strike;
+        }
+        
+        _stack.Push((price, strike));
+        return strike;
+    }
+}
