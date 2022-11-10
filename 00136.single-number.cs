96dd85886bb8d83a14c5921026dc5b2ac7c422c3
i00lii <dydaniil@yandex.ru>
2022-11-10 23:45:10 +0300

    Update 00136.single-number.cs

--- 00136.single-number.cs
+++ 00136.single-number.cs
@@ -1,2 +1,15 @@
+// https://leetcode.com/problems/single-number/description/
+public class Solution 
+{
+    public int SingleNumber(int[] nums) 
+    {
+        int result = 0;
 
-
+        foreach(int value in nums)
+        {
+            result ^= value;
+        }
+
+        return result;
+    }
+}
