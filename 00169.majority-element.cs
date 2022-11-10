c129bf5f9db69b5513f820488d0249fc76e1b7dc
i00lii <dydaniil@yandex.ru>
2022-11-10 21:28:17 +0300

    Update 00169.majority-element.cs

--- 00169.majority-element.cs
+++ 00169.majority-element.cs
@@ -1,2 +1,33 @@
+// https://leetcode.com/problems/majority-element/description/
+public class Solution 
+{
+    public int MajorityElement(int[] nums) 
+    {
+        int result = nums[0];
+        int count = 1; 
 
-
+        for(int idx = 1; idx < nums.Length; idx++)
+        {
+            int current = nums[idx];
+            
+            if (current == result)
+            {
+                count++;
+            }
+            else
+            {
+                if (count == 0)
+                {
+                    result = current;
+                    count = 1;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
