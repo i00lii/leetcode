c4b2ebbff7cf3fa9fa492c5b868477772e481cf8
i00lii <dydaniil@yandex.ru>
2022-11-10 13:16:06 +0300

    Update 01047.remove-all-adjacent-duplicates-in-string.cs

--- 01047.remove-all-adjacent-duplicates-in-string.cs
+++ 01047.remove-all-adjacent-duplicates-in-string.cs
@@ -1,2 +1,23 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string 
+public class Solution 
+{
+    public string RemoveDuplicates(string s) 
+    {
+        StringBuilder buffer = new StringBuilder(s);
 
-
+        for(int idx = 0; idx < buffer.Length - 1;)
+        {
+            if (buffer[idx] == buffer[idx + 1])
+            {
+                buffer.Remove(idx, 2);
+                idx = Math.Max(idx - 1, 0);
+            }
+            else 
+            {
+                idx++;
+            }
+        }
+
+        return buffer.ToString();
+    }
+}
