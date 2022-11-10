0c6504e77740b512c75fd2987df08461e0aaa483
i00lii <dydaniil@yandex.ru>
2022-11-10 19:50:33 +0300

    Update 00171.excel-sheet-column-number.cs

--- 00171.excel-sheet-column-number.cs
+++ 00171.excel-sheet-column-number.cs
@@ -1,2 +1,15 @@
+// https://leetcode.com/problems/excel-sheet-column-number/description/
+public class Solution 
+{
+    public int TitleToNumber(string columnTitle) 
+    {
+        int result = 0;
+        for (int idx = 0; idx < columnTitle.Length; idx++)
+        {
+            result *= 26;
+            result += columnTitle[idx] - 'A' + 1;
+        }
 
-
+        return result;
+    }
+}
