b3ae2ae741fd57a7f3cb6e85c9a2e1dff870db20
i00lii <dydaniil@yandex.ru>
2022-11-10 16:33:49 +0300

    Update 00144.binary-tree-preorder-traversal.cs

--- 00144.binary-tree-preorder-traversal.cs
+++ 00144.binary-tree-preorder-traversal.cs
@@ -1,2 +1,33 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) 
+    {
+        List<int> result = new();
+        Reccursion(root);
+        
+        return result;
 
-
+        void Reccursion(TreeNode node)
+        {
+            if (node == default)
+                return;
+
+            result.Add(node.val);
+            Reccursion(node.left);
+            Reccursion(node.right);
+        }
+    }
+}
