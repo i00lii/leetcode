635b1e0f75e9d68e7383416707cf09737d9b91eb
i00lii <dydaniil@yandex.ru>
2022-11-10 16:36:07 +0300

    Update 000145.binary-tree-postorder-traversal.cs

--- 000145.binary-tree-postorder-traversal.cs
+++ 000145.binary-tree-postorder-traversal.cs
@@ -1,2 +1,34 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public IList<int> PostorderTraversal(TreeNode root) 
+    {
+        List<int> result = new();
+        Reccursion(root);
+        
+        return result;
 
-
+        void Reccursion(TreeNode node)
+        {
+            if (node == default)
+                return;
+
+            Reccursion(node.left);
+            Reccursion(node.right);
+            result.Add(node.val);
+        }
+    }
+}
