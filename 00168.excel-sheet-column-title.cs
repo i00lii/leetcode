c4bf0da5f07b3d6eb18b891d7717123baf89529b
i00lii <dydaniil@yandex.ru>
2022-11-10 19:35:36 +0300

    Update 00168.excel-sheet-column-title.cs

--- 00168.excel-sheet-column-title.cs
+++ 00168.excel-sheet-column-title.cs
@@ -1,2 +1,17 @@
+// https://leetcode.com/problems/excel-sheet-column-title/description/
+public class Solution 
+{
+    public string ConvertToTitle(int columnNumber) 
+    {
+        const int size = 26;
+        StringBuilder builder = new StringBuilder();
+        while (columnNumber > 0)
+        {
+            int modulo = (columnNumber - 1) % size;
+            builder.Insert(0, (char)('A' + modulo));
+            columnNumber = (columnNumber - modulo) / 26;
+        }
 
-
+        return builder.ToString();
+    }
+}
