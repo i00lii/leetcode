fbaa8262157a4e50cee60aa0d99b4a37b3d54bab
i00lii <dydaniil@yandex.ru>
2022-11-12 02:48:35 +0300

    Update 00121.best-time-to-buy-and-sell-stock.cs

--- 00121.best-time-to-buy-and-sell-stock.cs
+++ 00121.best-time-to-buy-and-sell-stock.cs
@@ -1,2 +1,23 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+public class Solution 
+{
+    public int MaxProfit(int[] prices) 
+    {
+        int buy = prices[0];
+        int profit = 0;
 
-
+        foreach(int price in prices)
+        {    
+            if (price > buy)
+            {
+                profit = Math.Max(profit, price - buy);
+            }
+            else 
+            {
+                buy = price;
+            }
+        }
+
+        return profit;
+    }
+}
