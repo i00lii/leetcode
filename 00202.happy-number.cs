e95aa72717301ed0d6070ae3ae90726b09c23d28
i00lii <dydaniil@yandex.ru>
2022-11-11 15:28:24 +0300

    Update 00202.happy-number.cs

--- 00202.happy-number.cs
+++ 00202.happy-number.cs
@@ -1,2 +1,25 @@
+// https://leetcode.com/problems/happy-number/description/
+public class Solution 
+{
+    public bool IsHappy(int n)
+    {
+        (int slow, int fast) = (n, n);
+        do {(slow, fast) = (Next(slow), Next(Next(fast)));} while (slow != fast);
+        return slow == 1;
+    }
 
-
+    private static int Next(int number)
+    {
+        const int basis = 10;
+        int result = 0;
+
+        while (number > 0)
+        {
+            int modulo = number % basis;
+            result += modulo * modulo;
+            number /= basis;
+        }
+
+        return result;
+    }
+}
