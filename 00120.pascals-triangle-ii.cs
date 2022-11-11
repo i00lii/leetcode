02e281a1ebcc2794967da57f28f3da21a006290e
i00lii <dydaniil@yandex.ru>
2022-11-12 02:09:13 +0300

    Update 00120.pascals-triangle-ii.cs

--- 00120.pascals-triangle-ii.cs
+++ 00120.pascals-triangle-ii.cs
@@ -1,2 +1,21 @@
+// https://leetcode.com/problems/pascals-triangle-ii/description/
+public class Solution 
+{
+    public IList<int> GetRow(int numRows) 
+    {
+        int[] buffer = new int[numRows + 1];
+        
+        for (int row = 0; row <= numRows; row++)
+        {  
+            int idx = numRows - row;
+            buffer[idx++] = 1;
 
-
+            for (; idx <= numRows - 1; idx++)
+            {
+                buffer[idx] = buffer[idx] + buffer[idx + 1];
+            }
+        }
+
+        return buffer;
+    }
+}
