8d2e6d9c7173d3deadcb20c579ab8fa579f5c1a3
i00lii <dydaniil@yandex.ru>
2022-11-13 01:52:43 +0300

    Update 00191.number-of-1-bits.cs

--- 00191.number-of-1-bits.cs
+++ 00191.number-of-1-bits.cs
@@ -1,2 +1,16 @@
+// https://leetcode.com/problems/number-of-1-bits/description/
+public class Solution 
+{
+    public int HammingWeight(uint n) 
+    {
+        const int size = sizeof(uint) * 8;
+        
+        uint result = 0;
+        for (int idx = 0; idx < size; idx++)
+        {
+            result += (n >> idx) & 1;
+        }
 
-
+        return (int)result;
+    }
+}
