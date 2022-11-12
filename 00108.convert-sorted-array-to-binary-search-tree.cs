e08b2a4e09825d59ddace23542b621d37ee98f32
i00lii <dydaniil@yandex.ru>
2022-11-13 01:30:48 +0300

    Update 00108.convert-sorted-array-to-binary-search-tree.cs

--- 00108.convert-sorted-array-to-binary-search-tree.cs
+++ 00108.convert-sorted-array-to-binary-search-tree.cs
@@ -1,2 +1,28 @@
-
-
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public TreeNode SortedArrayToBST(int[] nums) => Rec(0, nums.Length - 1, nums);
+    
+    private static TreeNode Rec(int x, int y, int[] nums)
+    {
+        if (x > y) return null;
+        int m = (x + y) / 2;
+        TreeNode node = new TreeNode(nums[m]);
+        node.left = Rec(x, m - 1, nums);
+        node.right = Rec(m + 1, y, nums);
+        return node;
+    }
+}
