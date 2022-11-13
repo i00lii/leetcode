4d0fa6dc57286ba4b095dec313efc35bb25acbbf
i00lii <dydaniil@yandex.ru>
2022-11-13 18:58:31 +0300

    Update 00283.move-zeroes.cs

--- 00283.move-zeroes.cs
+++ 00283.move-zeroes.cs
@@ -4,18 +4,24 @@ public class Solution
     public void MoveZeroes(int[] nums) 
     {
         int offset = 0;
+        int length = 0;
         
         for (int idx = 0; idx < nums.Length; idx++)
         {
             if (nums[idx] == 0)
             {
                 offset++;
+                continue;
             }
-            else if (offset > 0)
+            
+            if (offset > 0)
             {
                 nums[idx - offset] = nums[idx];
-                nums[idx] = 0;
             }
+            
+            length++;
         }
+
+        Array.Fill(nums, 0, length, nums.Length - length);
     }
 }
