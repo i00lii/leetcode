00f4dd72f349e4e95e3de6719cf40a4ba3f0b211
i00lii <dydaniil@yandex.ru>
2022-11-13 20:11:21 +0300

    Update 00414.third-maximum-number.cs

--- 00414.third-maximum-number.cs
+++ 00414.third-maximum-number.cs
@@ -1,2 +1,49 @@
-
-
+public class Solution 
+{
+    public int ThirdMax(int[] nums) 
+    {
+        int? x = default;
+        int? z = default;
+        int? y = default;
+        
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            int current = nums[idx];
+            
+            if (!x.HasValue || x == current)
+            {
+                x = current;
+                continue;
+            }
+            
+            if (current > x)
+            {
+                z = y;
+                y = x;
+                x = current;
+                continue;
+            }
+            
+            if (!y.HasValue || y == current)
+            {
+                y = current;
+                continue;
+            }
+            
+            if (current > y)
+            {
+                z = y;
+                y = current;
+                continue;
+            } 
+            
+            if (!z.HasValue || current > z)
+            {
+                z = current;
+                continue;
+            }
+        }
+        
+        return z.HasValue ? z.Value : x.Value;
+    }
+}
