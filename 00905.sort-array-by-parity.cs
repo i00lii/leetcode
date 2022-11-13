429c945658bac78bef788be7328435a1d8549afe
i00lii <dydaniil@yandex.ru>
2022-11-13 19:09:07 +0300

    Update 00905.sort-array-by-parity.cs

--- 00905.sort-array-by-parity.cs
+++ 00905.sort-array-by-parity.cs
@@ -1,2 +1,36 @@
-
-
+// https://leetcode.com/problems/sort-array-by-parity/description/
+public class Solution 
+{
+    public int[] SortArrayByParity(int[] nums) 
+    {
+        
+        int x = 0;
+        int y = nums.Length - 1;
+        
+        while (y > x)
+        {
+            int valueX = nums[x];
+            int valueY = nums[y];
+            
+            if (valueX % 2 == 0)
+            {
+                x++;
+                continue;
+            }
+            
+            if (valueY % 2 == 1)
+            {
+                y--;
+                continue;
+            }
+            
+            nums[x] = valueY;
+            nums[y] = valueX;
+            
+            y--;
+            x++;   
+        }
+        
+        return nums;
+    }
+}
