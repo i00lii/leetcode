6d3f97a6f24bcc7b9be8da008c18aa5c9662d0ab
i00lii <dydaniil@yandex.ru>
2022-11-13 18:25:49 +0300

    Update 00941.valid-mountain-array.cs

--- 00941.valid-mountain-array.cs
+++ 00941.valid-mountain-array.cs
@@ -1,2 +1,26 @@
+// https://leetcode.com/problems/valid-mountain-array/description/
+public class Solution 
+{
+    public bool ValidMountainArray(int[] arr) 
+    {
+        if (arr.Length < 3 || arr[0] >= arr[1] || arr[arr.Length - 2] <= arr[arr.Length -1])
+            return false;
+        
+        int peaks = 0;
+        
+        for(int x = 0; x < arr.Length - 2; x++)
+        {
+            int xValue = arr[x];
+            int yValue = arr[x + 1];
+            int zValue = arr[x + 2];
+            
+            if (xValue == yValue || zValue == yValue)
+                return false;
 
-
+            if (yValue > xValue && yValue > zValue)
+                peaks++;
+        }
+        
+        return peaks == 1;
+    }
+}
