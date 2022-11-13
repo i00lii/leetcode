e53fe5e248d9b7631ed41954092fa4feb09cd4b8
i00lii <dydaniil@yandex.ru>
2022-11-14 02:32:20 +0300

    Update 00033.search-in-rotated-sorted-array.cs

--- 00033.search-in-rotated-sorted-array.cs
+++ 00033.search-in-rotated-sorted-array.cs
@@ -3,58 +3,50 @@ public class Solution
 {
     public int Search(int[] nums, int target) 
     {
-        if (nums.Length == 1)
-            return nums[0] == target ? 0 : -1;
-
-        // no rotation
-        if (nums[0] < nums[nums.Length - 1])
-        {
-            int r = Array.BinarySearch(nums, target);
-            return r >= 0 ? r : -1;    
-        }
-
-        int rotationIdx = FindRoatationIdx(nums);
-        int nextIdx = rotationIdx + 1;
-        
-        if (target >= nums[0])
-        {
-            int r = Array.BinarySearch(nums, 0, nextIdx, target);
-            return r >= 0 ? r : -1;
-        }
-        else 
-        {
-            int r = Array.BinarySearch(nums, nextIdx, nums.Length - nextIdx, target);
-            return r >= 0 ? r : -1;
-        }
-    }
-
-    private static int FindRoatationIdx(int[] nums)
-    {
         int l = 0;
         int r = nums.Length - 1;
 
-        while (r - l > 1)
+        while (r >= l)
         {
+            int m = l + (r - l) / 2;
+            
             int valueL = nums[l];
             int valueR = nums[r];
-
-            int m = l + (r - l) / 2;
             int valueM = nums[m];
+
+            if (valueM == target)
+                return m;
+
+            if (valueL == target)
+                return l;
+
+            if (valueR == target)
+                return r;
 
             if (valueM < valueR)
             {
-                r = m;
-                continue;
+                if (target > valueM && target < valueR)
+                {
+                    l = m + 1;
+                }
+                else 
+                {
+                    r = m - 1;
+                }
             }
-
-            if (valueM > valueL)
+            else
             {
-                l = m;
-                continue;
+                if (target < valueM && target > valueL)
+                {
+                    r = m - 1;
+                }
+                else 
+                {
+                    l = m + 1;
+                }
             }
         }
 
-        return l;
+        return -1;
     }
-
 }
