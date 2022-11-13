62263dfd4f1f54f206fc72ea6fac5efca95c6426
i00lii <dydaniil@yandex.ru>
2022-11-13 17:04:00 +0300

    Update 00028.find-the-index-of-the-first-occurrence-in-a-string.cs

--- 00028.find-the-index-of-the-first-occurrence-in-a-string.cs
+++ 00028.find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -1,2 +1,28 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+//
+// The solution is not optimal.
+// The faster solution in to use Z-algorithm with O(n + m) time complexity 
+// but also O(n + m) additional space required.
+public class Solution 
+{
+    public int StrStr(string haystack, string needle) 
+    {
+        for (int idx = 0; idx <= haystack.Length - needle.Length; idx++)
+        {
+            if (AreEquals(haystack, idx, needle) return idx;
+        }
 
-
+        return -1;
+    }
+
+    private static bool AreEquals(string haystack, int idx, string needle)
+    {
+        for(int jdx = 0; jdx < needle.Length; jdx++)
+        {
+            if (haystack[idx + jdx] != needle[jdx])
+                return false;
+        }
+
+        return true;
+    }
+}
