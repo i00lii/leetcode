392c75683bcc0a93b886707fd319110510dcd68d
i00lii <dydaniil@yandex.ru>
2022-11-13 17:07:10 +0300

    Update 01480.running-sum-of-1d-array.cs

--- 01480.running-sum-of-1d-array.cs
+++ 01480.running-sum-of-1d-array.cs
@@ -1,2 +1,16 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/description
+public class Solution 
+{
+    public int[] RunningSum(int[] nums)
+    {
+        int total = 0;
 
-
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            total += nums[idx];
+            nums[idx] = total;
+        }   
+
+        return nums;
+    }
+}
