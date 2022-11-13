56c7f686e77ba3172f0befc46b261c0287672188
i00lii <dydaniil@yandex.ru>
2022-11-13 18:51:31 +0300

    Update 00283.move-zeroes.cs

--- 00283.move-zeroes.cs
+++ 00283.move-zeroes.cs
@@ -1,2 +1,21 @@
-
-
+// https://leetcode.com/problems/move-zeroes/description/
+public class Solution
+{
+    public void MoveZeroes(int[] nums) 
+    {
+        int offset = 0;
+        
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            if (nums[idx] == 0)
+            {
+                offset++;
+            }
+            else if (offset > 0)
+            {
+                nums[idx - offset] = nums[idx];
+                nums[idx] = 0;
+            }
+        }
+    }
+}
