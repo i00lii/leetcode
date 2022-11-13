1e4abbb1fa743f3ee6b894096a64c4c0015063f1
i00lii <dydaniil@yandex.ru>
2022-11-13 17:40:07 +0300

    Update 01346.check-if-n-and-its-double-exist.cs

--- 01346.check-if-n-and-its-double-exist.cs
+++ 01346.check-if-n-and-its-double-exist.cs
@@ -1,2 +1,25 @@
-
-
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+public class Solution 
+{
+    public bool CheckIfExist(int[] arr) 
+    {
+        HashSet<int> buffer = new();
+        
+        for (int idx = 0; idx < arr.Length; idx++)
+        {
+            int current = arr[idx];
+            
+            if (buffer.Contains(current * 2)) return true;
+            
+            if (current % 2 == 0)
+            {
+                int down = current / 2;
+                if (buffer.Contains(down)) return true;
+            }
+            
+            buffer.Add(current);
+        }
+        
+        return false;
+    }
+}
