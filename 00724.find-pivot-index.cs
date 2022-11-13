2576932d73e2fff1801f8f073a88879ac1dc8de6
i00lii <dydaniil@yandex.ru>
2022-11-13 17:30:02 +0300

    Update 00724.find-pivot-index.cs

--- 00724.find-pivot-index.cs
+++ 00724.find-pivot-index.cs
@@ -1,2 +1,31 @@
+// https://leetcode.com/problems/find-pivot-index/description
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int total = 0;
+        
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            total += nums[idx];
+        }    
 
-
+        int left = 0;
+        int right = total;
+
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            int current = nums[idx];
+            right -= current; 
+
+            if (left == right)
+            {
+                return idx;
+            }
+
+            left += current;
+        }
+
+        return -1;
+    }
+}
