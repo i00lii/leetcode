ffc77a6655f23b5bd21c5b27b7e4b2d8f5fd88e7
i00lii <dydaniil@yandex.ru>
2022-11-13 18:33:44 +0300

    Update 01299.replace-elements-with-greatest-element-on-right-side.cs

--- 01299.replace-elements-with-greatest-element-on-right-side.cs
+++ 01299.replace-elements-with-greatest-element-on-right-side.cs
@@ -1,2 +1,18 @@
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+public class Solution 
+{
+    public int[] ReplaceElements(int[] arr) 
+    {
+        int popped = -1;
 
-
+        for (int idx = arr.Length - 1; idx >= 0; idx--)
+        {
+            int current = arr[idx];
+            arr[idx] = popped;
+            
+            popped = Math.Max(current, popped);
+        }
+        
+        return arr;
+    }
+}
