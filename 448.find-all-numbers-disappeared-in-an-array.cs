01fef244debb9e758d5dfe5b299cfb54592cc3e0
i00lii <dydaniil@yandex.ru>
2022-11-13 23:32:34 +0300

    Update 448.find-all-numbers-disappeared-in-an-array.cs

--- 448.find-all-numbers-disappeared-in-an-array.cs
+++ 448.find-all-numbers-disappeared-in-an-array.cs
@@ -1,2 +1,21 @@
-
-
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+public class Solution 
+{
+    public IList<int> FindDisappearedNumbers(int[] nums) 
+    {
+        List<int> result = new();
+        
+        foreach (int i in nums)
+        {
+            ref int x = ref nums[Math.Abs(i) - 1];
+            x = -Math.Abs(x);
+        }
+        
+        for (int idx = 0; idx < nums.Length; idx++)
+        {
+            if (nums[idx] > 0) result.Add(idx + 1);
+        }
+        
+        return result;
+    }
+}
