cd13a2a43cd9fa591c6aca5b45d525dc7aa1c54c
i00lii <dydaniil@yandex.ru>
2022-11-13 20:17:47 +0300

    Update 01051.height-checker.cs

--- 01051.height-checker.cs
+++ 01051.height-checker.cs
@@ -1,2 +1,21 @@
-
-
+// https://leetcode.com/problems/height-checker/description/
+public class Solution 
+{
+    public int HeightChecker(int[] heights) 
+    {
+        int[] buffer = new int[heights.Length];
+        Array.Copy(heights, 0, buffer, 0, buffer.Length);
+        Array.Sort(buffer);
+        
+        int count = 0;
+        for (int idx = 0; idx < buffer.Length; idx++)
+        {
+            if (heights[idx] != buffer[idx])
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
