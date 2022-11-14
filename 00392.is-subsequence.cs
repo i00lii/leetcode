2b92cd9dbc60c317d966e7b2b79295ac846fec90
i00lii <dydaniil@yandex.ru>
2022-11-14 21:11:54 +0300

    Update 00392.is-subsequence.cs

--- 00392.is-subsequence.cs
+++ 00392.is-subsequence.cs
@@ -1,2 +1,17 @@
+// https://leetcode.com/problems/is-subsequence/description
+public class Solution 
+{
+    public bool IsSubsequence(string s, string t) 
+    {
+        int sIdx = 0;
+        int tIdx = 0;
 
-
+        while (sIdx < s.Length && tIdx < t.Length)
+        {
+            if (s[sIdx] == t[tIdx]) sIdx++;
+            tIdx++;
+        }
+
+        return sIdx == s.Length;
+    }
+}
