c9ec2d1c9cdc27e50273be9aea9d12f963fa01f3
i00lii <dydaniil@yandex.ru>
2022-11-14 21:10:29 +0300

    Update 00205.isomorphic-strings.cs

--- 00205.isomorphic-strings.cs
+++ 00205.isomorphic-strings.cs
@@ -1,2 +1,29 @@
+public class Solution 
+{
+    public bool IsIsomorphic(string s, string t) 
+    {
+        char[] mapST = new char[128];
+        char[] mapTS = new char[128];
 
-
+        for (int i = 0; i < s.Length; i++)
+        {
+            char valueS = s[i];
+            char valueT = t[i];
+
+            if (mapST[valueS] == 0 && mapTS[valueT] == 0)
+            {
+                mapST[valueS] = valueT;
+                mapTS[valueT] = valueS;
+            }
+            else
+            {
+                if (mapST[valueS] != valueT || mapTS[valueT] != valueS)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true; 
+    }
+}
