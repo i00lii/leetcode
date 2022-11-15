c462ee38498e8496208b3b4f7cd0ad8ddb82efd3
i00lii <dydaniil@yandex.ru>
2022-11-16 01:00:45 +0300

    Update 00222.count-complete-tree-nodes.cs

--- 00222.count-complete-tree-nodes.cs
+++ 00222.count-complete-tree-nodes.cs
@@ -1,2 +1,35 @@
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public int CountNodes(TreeNode root)
+    {
+        int l = Count(root, x => x.left);
+        int r = Count(root, x => x.right);
+        return l == r ? (int)Math.Pow(2, l) - 1 : 1 + CountNodes(root.left) + CountNodes(root.right); 
+    }
 
-
+    private static int Count(TreeNode root, Func<TreeNode, TreeNode> next)
+    {
+        int count = 0;
+        while (root != default)
+        {
+            count++;
+            root = next(root);
+        }
+
+        return count;
+    }   
+}
