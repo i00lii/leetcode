f4d5c617dc880b329e826b4fb9649e9cf4234605
i00lii <dydaniil@yandex.ru>
2022-11-16 23:03:12 +0300

    Update 00049.group-anagrams.cs

--- 00049.group-anagrams.cs
+++ 00049.group-anagrams.cs
@@ -1,2 +1,26 @@
+// https://leetcode.com/problems/group-anagrams/description/
+public class Solution 
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        Dictionary<string, List<string>> buffer = new();
 
-
+        foreach (string value in strs)
+        {
+            char[] allocation = value.ToCharArray();
+            Array.Sort(allocation);
+            string key = new string(allocation);
+
+            if (buffer.TryGetValue(key, out List<string> anagrams))
+            {
+                anagrams.Add(value);
+            }
+            else 
+            {
+                buffer.Add(key, new List<string>(){value});
+            }
+        }
+
+        return buffer.Values.ToList<IList<string>>();
+    }
+}
