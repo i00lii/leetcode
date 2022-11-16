401d9b5544607d4781dc36203e70718386f8a81f
i00lii <dydaniil@yandex.ru>
2022-11-16 17:24:53 +0300

    Update 00876.middle-of-the-linked-list.cs

--- 00876.middle-of-the-linked-list.cs
+++ 00876.middle-of-the-linked-list.cs
@@ -1,2 +1,29 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 
-
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode l = head;
+        ListNode r = head;
+
+        while (r?.next != default)
+        {
+            l = l.next;
+            r = r.next.next;
+        }
+
+        return l;
+    }
+}
