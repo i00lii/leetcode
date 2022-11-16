cd7c6611537ad8209d387c1be9555ca147ff19e7
i00lii <dydaniil@yandex.ru>
2022-11-16 17:23:20 +0300

    Update 00374.guess-number-higher-or-lower.cs

--- 00374.guess-number-higher-or-lower.cs
+++ 00374.guess-number-higher-or-lower.cs
@@ -1,2 +1,30 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/description/
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
 
-
+public class Solution : GuessGame 
+{
+    public int GuessNumber(int n)
+    {
+        int l = 1;
+        int r = n;
+
+        while (r >= l)
+        {
+            int m = l + (r - l) / 2;
+            int g = guess(m);
+
+            if (g == 0) return m;
+            else if (g > 0) l = m + 1;
+            else r = m - 1;
+        }
+
+        return l;
+    }
+}
