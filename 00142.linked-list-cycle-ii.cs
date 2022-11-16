a2a5a74f6321d0bb84f908f0cc862c8219091f5e
i00lii <dydaniil@yandex.ru>
2022-11-16 17:26:14 +0300

    Update 00142.linked-list-cycle-ii.cs

--- 00142.linked-list-cycle-ii.cs
+++ 00142.linked-list-cycle-ii.cs
@@ -1,2 +1,40 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/description
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public ListNode DetectCycle(ListNode head) 
+    {
+        ListNode x = head;
+        ListNode y = head;
 
-
+        while (y?.next != default)
+        {
+            x = x.next;
+            y = y.next.next;
+
+            if (x == y)
+            {
+                y = head;
+                while(x != y)
+                {
+                    x = x.next;
+                    y = y.next;
+                }
+
+                return x;
+            }
+        }
+
+        return default;
+    }
+}
