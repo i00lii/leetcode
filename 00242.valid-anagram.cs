1c5cd4c0663ac87cd16e55d313d35241ee6eee2a
i00lii <dydaniil@yandex.ru>
2022-11-16 22:53:34 +0300

    Update 00242.valid-anagram.cs

--- 00242.valid-anagram.cs
+++ 00242.valid-anagram.cs
@@ -1,2 +1,34 @@
+// https://leetcode.com/problems/valid-anagram/description/
+public class Solution 
+{
+    private static readonly int[] _counter = new int['z' - 'a' + 1];
 
-
+    public bool IsAnagram(string s, string t)
+    {   
+        if (s.Length != t.Length) return false;
+        Array.Fill(_counter, 0);
+        
+        foreach (char x in s)
+        {
+            _counter[x - 'a']++;
+        }
+
+        int balance = s.Length;
+        foreach (char x in t)
+        {
+            ref int c = ref _counter[x - 'a'];
+
+            if (c > 0)
+            {
+                balance--;
+                c--;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        return balance == 0;
+    }
+}
