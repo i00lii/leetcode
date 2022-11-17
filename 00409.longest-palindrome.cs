a046e38f3246f6c660ea60692ae7daa21a15c2d9
i00lii <dydaniil@yandex.ru>
2022-11-17 22:26:40 +0300

    Update 00409.longest-palindrome.cs

--- 00409.longest-palindrome.cs
+++ 00409.longest-palindrome.cs
@@ -1,2 +1,29 @@
+// https://leetcode.com/problems/longest-palindrome/description/
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        HashSet<char> buffer = new();
+        int result = 0;
 
-
+        foreach (char value in s)
+        {
+            if (!buffer.Contains(value))
+            {
+                buffer.Add(value);
+            }
+            else 
+            {
+                buffer.Remove(value);
+                result += 2;
+            }
+        }
+
+        if (buffer.Count > 0)
+        {
+            result += 1;
+        }
+
+        return result;
+    }
+}
