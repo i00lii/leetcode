7101888aa50843ebc45b69d8328081413bcd102a
i00lii <dydaniil@yandex.ru>
2022-11-17 22:31:54 +0300

    Update 00053.maximum-subarray.cs

--- 00053.maximum-subarray.cs
+++ 00053.maximum-subarray.cs
@@ -1,2 +1,17 @@
+// https://leetcode.com/problems/maximum-subarray/description/
+public class Solution 
+{
+    public int MaxSubArray(int[] nums) 
+    {
+        int max = int.MinValue;
+        int sum = 0;
 
-
+        foreach (int n in nums)
+        {
+            sum = Math.Max(n, sum + n);
+            max = Math.Max(max, sum);
+        }
+
+        return max;
+    }
+}
