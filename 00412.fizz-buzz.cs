c4a1ac2d1ecca433a7a6f0808336dcc30ac235d0
i00lii <dydaniil@yandex.ru>
2022-11-19 02:22:15 +0300

    Update 00412.fizz-buzz.cs

--- 00412.fizz-buzz.cs
+++ 00412.fizz-buzz.cs
@@ -1,2 +1,20 @@
+// https://leetcode.com/problems/fizz-buzz/description/
+public class Solution 
+{
+    public IList<string> FizzBuzz(int n) 
+    {
+        List<string> result = new (n);
 
-
+        for (int i = 1; i <= n; i++)
+        {
+            string r = string.Empty;
+            
+            if (i % 3 == 0) r += "Fizz";
+            if (i % 5 == 0) r += "Buzz";
+
+            result.Add(string.IsNullOrEmpty(r) ? i.ToString() : r);
+        }
+
+        return result;
+    }
+}
