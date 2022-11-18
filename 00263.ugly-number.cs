b1a68ca8707dce63485bb7bfc839b2240c7af813
i00lii <dydaniil@yandex.ru>
2022-11-18 21:57:43 +0300

    Update 00263.ugly-number.cs

--- 00263.ugly-number.cs
+++ 00263.ugly-number.cs
@@ -1,2 +1,13 @@
+// https://leetcode.com/problems/ugly-number/description/
+public class Solution 
+{
+    public bool IsUgly(int n)
+    {
+        if (n <= 0) return false;
+        while (n % 5 == 0) n = n / 5;
+        while (n % 3 == 0) n = n / 3;
+        while (n % 2 == 0) n = n / 2;
 
-
+        return n == 1;
+    }
+}
