d7c3205284507d78a72c1a8abfd300fb973bc608
i00lii <dydaniil@yandex.ru>
2022-11-18 22:23:12 +0300

    Update 00055.jump-game.cs

--- 00055.jump-game.cs
+++ 00055.jump-game.cs
@@ -1,2 +1,18 @@
+// https://leetcode.com/problems/jump-game/description/
+public class Solution 
+{
+    public bool CanJump(int[] nums)
+    {
+        int target = nums.Length - 1;
 
-
+        for (int index = nums.Length - 1; index >= 0; index--)
+        {
+            if (nums[index] + index >= target)
+            {
+                target = index;
+            }
+        }
+
+        return (target == 0);
+    }
+}
