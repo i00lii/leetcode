33e7cd45229740124c72d2b4fecc28c04d277d92
i00lii <dydaniil@yandex.ru>
2022-11-19 12:27:48 +0300

    Update 00278.first-bad-version.cs

--- 00278.first-bad-version.cs
+++ 00278.first-bad-version.cs
@@ -1,2 +1,27 @@
+// https://leetcode.com/problems/first-bad-version/description
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
 
-
+public class Solution : VersionControl 
+{
+    public int FirstBadVersion(int n) 
+    {
+        int l = 1;
+        int r = n;
+
+        while (r > l)
+        {
+            int m = l + (r - l) / 2;
+
+            if (IsBadVersion(m))
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
