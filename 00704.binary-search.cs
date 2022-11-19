2a500af83639e1eab6138eb7a2a05905799e5e63
i00lii <dydaniil@yandex.ru>
2022-11-19 12:20:13 +0300

    Update 00704.binary-search.cs

--- 00704.binary-search.cs
+++ 00704.binary-search.cs
@@ -1,1 +1,21 @@
+// https://leetcode.com/problems/binary-search/description
+public class Solution 
+{
+    public int Search(int[] nums, int target) 
+    {
+        int l = 0;
+        int r = nums.Length - 1;
 
+        while (r >= l)
+        {
+            int m = l + (r - l) / 2;
+            int valueM = nums[m];
+
+            if (valueM == target) return m;
+            else if (valueM > target) r = m - 1;
+            else l = m + 1;
+        }
+
+        return -1;
+    }
+}
