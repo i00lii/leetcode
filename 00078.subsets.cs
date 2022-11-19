48210ff08771dc0869887fc5ef48501906562f82
i00lii <dydaniil@yandex.ru>
2022-11-20 01:54:18 +0300

    Update 00078.subsets.cs

--- 00078.subsets.cs
+++ 00078.subsets.cs
@@ -1,2 +1,26 @@
+// https://leetcode.com/problems/subsets/description/
+using System.Collections.Immutable;
 
-
+public class Solution 
+{
+    public IList<IList<int>> Subsets(int[] nums) 
+    {
+        List<IList<int>> result = new ();
+        ImmutableArray<int> buffer = ImmutableArray<int>.Empty;
+        result.Add(buffer);
+        
+        Generate(nums, 0, buffer, result);
+        return result;
+    }
+    
+    private static void Generate(int[] nums, int start, ImmutableArray<int> buffer, List<IList<int>> result)
+    {
+        for (int idx = start; idx < nums.Length; idx++)
+        {
+            ImmutableArray<int> current = buffer.Add(nums[idx]);
+            result.Add(current);
+
+            Generate(nums, idx + 1, current, result);
+        }
+    }
+}
