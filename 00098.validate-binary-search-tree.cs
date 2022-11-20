2748e6b5ac5c9041ba20152aef14b7c59dcfab79
i00lii <dydaniil@yandex.ru>
2022-11-20 19:38:12 +0300

    Update 00098.validate-binary-search-tree.cs

--- 00098.validate-binary-search-tree.cs
+++ 00098.validate-binary-search-tree.cs
@@ -1,2 +1,24 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public bool IsValidBST(TreeNode root) 
+        => Reccursion(root, long.MinValue, long.MaxValue);
 
-
+    private static bool Reccursion(TreeNode node, long min, long max)
+        => node == default ? true
+        : node.val <= min || node.val >= max ? false
+        : Reccursion(node.left, min, node.val) && Reccursion(node.right, node.val, max);
+}
