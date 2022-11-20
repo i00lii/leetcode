5495666b4982a010e28933512ec6cd951204eb1a
i00lii <dydaniil@yandex.ru>
2022-11-21 00:59:42 +0300

    Update 01476.subrectangle-queries.cs

--- 01476.subrectangle-queries.cs
+++ 01476.subrectangle-queries.cs
@@ -2,18 +2,33 @@
 public class SubrectangleQueries 
 {
     private readonly int[][] _rectangle;
-    public SubrectangleQueries(int[][] rectangle) => _rectangle = rectangle;
+    private readonly List<(int RowX, int RowY, int ColX, int ColY, int Value)> _override;
+
+    public SubrectangleQueries(int[][] rectangle)
+    {
+        _rectangle = rectangle;
+        _override = new List<(int, int, int, int, int)>();
+    } 
     
     public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
     {
-        for (int row = row1; row <= row2; row++)
-        {
-            int[] rowValues = _rectangle[row];
-            Array.Fill(rowValues, newValue, col1, col2 - col1 + 1);
-        }
+        _override.Add((row1, row2, col1, col2, newValue));
     }
     
-    public int GetValue(int row, int col) => _rectangle[row][col];
+    public int GetValue(int row, int col)
+    {
+        for(int idx = _override.Count - 1;idx >= 0; idx--)
+        {
+            (int rowX, int rowY, int colX, int colY, int value) = _override[idx];
+
+            if (Fits(rowX, rowY, row) && Fits(colX, colY, col))
+                return value;
+        }
+
+        return _rectangle[row][col];
+    }
+
+    private static bool Fits(int x, int y, int target) => target >= x && target <= y;
 }
 
 /**
@@ -22,4 +37,3 @@ public class SubrectangleQueries
  * obj.UpdateSubrectangle(row1,col1,row2,col2,newValue);
  * int param_2 = obj.GetValue(row,col);
  */
-
