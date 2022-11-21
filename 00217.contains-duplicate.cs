ebc6e3470d78a3c7154ba98b92c3276f54b56214
i00lii <dydaniil@yandex.ru>
2022-11-21 18:47:12 +0300

    Update 00217.contains-duplicate.cs

--- 00217.contains-duplicate.cs
+++ 00217.contains-duplicate.cs
@@ -1,2 +1,16 @@
+// https://leetcode.com/problems/contains-duplicate/description
+public class Solution 
+{
+    public bool ContainsDuplicate(int[] nums) 
+    {
+        Array.Sort(nums);
 
-
+        for (int idx = 0; idx < nums.Length - 1; idx++)
+        {
+            if (nums[idx] == nums[idx + 1])
+                return true;
+        }
+
+        return false;
+    }
+}
