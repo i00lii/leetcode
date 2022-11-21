533823ba0c2bc0e8838819cc44d73e1eb3458ca9
i00lii <dydaniil@yandex.ru>
2022-11-21 23:01:48 +0300

    Update 00039.combination-sum.cs

--- 00039.combination-sum.cs
+++ 00039.combination-sum.cs
@@ -1,2 +1,43 @@
+// https://leetcode.com/problems/combination-sum/
+public class Solution 
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target) 
+    {
+        List<IList<int>> result = new ();
+        Generate(candidates, 0, target, new List<int>(), result);
+        return result;
+    }
 
-
+    private static void Generate
+    (
+        int[] candidates,
+        int offset,
+        int target,
+        List<int> buffer,
+        List<IList<int>> result
+    )
+    {
+        if (target == 0)
+        {
+            result.Add(buffer.ToArray());
+            return;
+        }
+
+        for(; offset < candidates.Length; offset++)
+        {
+            int number = candidates[offset];
+            int maxK = target / number;
+
+            if (maxK > 0)
+            {
+                for (int k = 1; k <= maxK; k++)
+                {
+                    buffer.Add(number);
+                    Generate(candidates, offset + 1, target - k * number, buffer, result); 
+                }
+
+                buffer.RemoveRange(buffer.Count - maxK, maxK);
+            }
+        }
+    }
+}
