295ae5fad0b346b1fef0ba5ba53bab2c7a9bbb48
i00lii <dydaniil@yandex.ru>
2022-11-22 16:05:29 +0300

    Update 00509.fibonacci-number.cs

--- 00509.fibonacci-number.cs
+++ 00509.fibonacci-number.cs
@@ -1,2 +1,18 @@
+// https://leetcode.com/problems/fibonacci-number/description
+public class Solution 
+{
+    private readonly int[] _lookup;
 
-
+    public Solution()
+    {
+        _lookup = new int[31];
+        _lookup[1] = 1;
+
+        for (int idx = 2; idx < _lookup.Length; idx++)
+        {
+            _lookup[idx] = _lookup[idx - 1] + _lookup[idx - 2];
+        }
+    }
+
+    public int Fib(int n) => _lookup[n];
+}
