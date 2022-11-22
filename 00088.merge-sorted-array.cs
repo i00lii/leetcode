ac23b7449b824158b2144372bf190c30b085e976
i00lii <dydaniil@yandex.ru>
2022-11-22 16:07:14 +0300

    Update 00088.merge-sorted-array.cs

--- 00088.merge-sorted-array.cs
+++ 00088.merge-sorted-array.cs
@@ -1,2 +1,33 @@
+// https://leetcode.com/problems/merge-sorted-array/description
+public class Solution 
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int idxM = m - 1;
+        int idxN = n - 1;
+        
+        int idxR = nums1.Length - 1;
 
-
+        while (idxM >= 0 && idxN >= 0)
+        {
+            int valueM = nums1[idxM];
+            int valueN = nums2[idxN];
+            
+            if (valueM > valueN)
+            {
+                nums1[idxR--] = valueM;
+                idxM--;
+            }
+            else 
+            {
+                nums1[idxR--] = valueN;
+                idxN--;
+            }
+        }
+
+        for (;idxN >= 0; idxN--)
+        {
+            nums1[idxR--] = nums2[idxN];
+        }
+    }
+}
