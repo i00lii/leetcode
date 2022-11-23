0b5874f7f022019695fd13749aa865d6fd841fe9
i00lii <dydaniil@yandex.ru>
2022-11-23 20:58:54 +0300

    Update 00746.min-cost-climbing-stairs.cs

--- 00746.min-cost-climbing-stairs.cs
+++ 00746.min-cost-climbing-stairs.cs
@@ -1,2 +1,20 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/description/
+public class Solution 
+{
+    public int MinCostClimbingStairs(int[] cost) 
+    {
+        int priceMinus1 = 0;
+        int priceMinus2 = 0;
 
-
+        for (int idx = 2; idx <= cost.Length; idx++)
+        {
+            int p2 = priceMinus2 + cost[idx - 2];
+            int p1 = priceMinus1 + cost[idx - 1];
+
+            priceMinus2 = priceMinus1;
+            priceMinus1 = Math.Min(p1, p2);
+        }
+
+        return priceMinus1;
+    }
+}
