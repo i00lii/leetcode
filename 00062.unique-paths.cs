177588426482133134e8197d07d6d5facb6b5cdc
i00lii <dydaniil@yandex.ru>
2022-11-23 20:59:58 +0300

    Update 00062.unique-paths.cs

--- 00062.unique-paths.cs
+++ 00062.unique-paths.cs
@@ -1,2 +1,19 @@
+// https://leetcode.com/problems/unique-paths/description/
+public class Solution 
+{
+    public int UniquePaths(int m, int n) 
+    {
+        int[] buffer = new int[n + 1];
+        buffer[1] = 1;
+        
+        for (int mIdx = 0; mIdx < m; mIdx++)
+        {
+            for (int nIdx = 1; nIdx <= n; nIdx++)
+            {                
+                buffer[nIdx] = buffer[nIdx - 1] + buffer[nIdx];
+            }
+        }
 
-
+        return buffer[n];
+    }
+}
