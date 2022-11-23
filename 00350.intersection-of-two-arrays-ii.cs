228165fa12d9984ac511868c845fefb27d65d0d9
i00lii <dydaniil@yandex.ru>
2022-11-23 20:57:53 +0300

    Update 00350.intersection-of-two-arrays-ii.cs

--- 00350.intersection-of-two-arrays-ii.cs
+++ 00350.intersection-of-two-arrays-ii.cs
@@ -1,2 +1,36 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+public class Solution 
+{
+    public int[] Intersect(int[] nums1, int[] nums2) 
+    {
+        (nums1, nums2) = nums1.Length < nums2.Length ? (nums1, nums2) : (nums2, nums1);
 
-
+        int max = nums1.Length;
+        Dictionary<int, int> buffer = new ();
+
+        foreach (int number in nums1)
+        {
+            if (!buffer.ContainsKey(number)) buffer[number] = 1;
+            else buffer[number]++;
+        }
+
+        List<int> result = new ();
+        foreach (int number in nums2)
+        {
+            if (max <= 0) break;
+
+            if (buffer.ContainsKey(number))
+            {
+                int count = buffer[number] - 1;
+                result.Add(number);
+
+                if (count > 0) buffer[number] = count;
+                else buffer.Remove(number);
+
+                max--;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
