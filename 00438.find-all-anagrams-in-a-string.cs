980f46e316e0e8a0dad9085bc6d9224ba784753a
i00lii <dydaniil@yandex.ru>
2022-11-24 21:57:24 +0300

    Update 00438.find-all-anagrams-in-a-string.cs

--- 00438.find-all-anagrams-in-a-string.cs
+++ 00438.find-all-anagrams-in-a-string.cs
@@ -1,2 +1,28 @@
+// https://leetcode.com/problems/find-all-anagrams-in-a-string/description
+public class Solution 
+{
+    public IList<int> FindAnagrams(string s, string p) 
+    {   
+        List<int> result = new ();
+        if (s.Length < p.Length) return result;
 
-
+        char[] buffer = new char[26];
+        foreach(char valueP in p) buffer[valueP - 'a']++;
+        
+        for (int idx = 0, jdx = -p.Length; idx < s.Length; idx++, jdx++)
+        {
+            buffer[s[idx] - 'a']--;
+            if (jdx >= 0) buffer[s[jdx] - 'a']++;
+            if (Empty(buffer)) result.Add(jdx + 1);
+        }
+
+        return result;
+    }
+
+    private static bool Empty(char[] buffer)
+    {
+        int sum = 0;
+        foreach(int v in buffer) sum += v;
+        return sum == 0;
+    }
+}
